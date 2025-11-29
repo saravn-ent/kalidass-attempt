67f46ff6c41593f6adecf8b209fdf20ba4b91c74
SARAVN <saravn.ent@gmail.com>
2025-11-29 15:59:13 +0530

    update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      colors: {
+        'android-blue': '#4285F4',
+        'android-dark': '#3367D6',
+      },
       fontFamily: {
         sans: ['"Noto Sans"', '"Noto Sans Tamil"', 'sans-serif'],
       },
